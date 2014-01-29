62212d23616236a8e2c40c23ab6ce73ae89fefa7
NovemberFoxtrot <november@fox.trot>
2014-01-29 01:18:42 -0800

    hmm

--- gadshill.go
+++ gadshill.go
@@ -1,78 +1,97 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
 	"os"
+	"time"
 	"unicode"
 )
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+type Config struct {
+	ConnectTimeout   time.Duration
+	ReadWriteTimeout time.Duration
+}
 
-	for scanner.Scan() {
-		someline := scanner.Text()
+func TimeoutDialer(cTimeout time.Duration, rwTimeout time.Duration) func(net, addr string) (c net.Conn, err error) {
+	return func(netw, addr string) (net.Conn, error) {
+		conn, err := net.DialTimeout(netw, addr, cTimeout)
 
-		for _, r := range someline {
-			fmt.Printf("%c ", r)
-
-			if unicode.IsControl(r) {
-				fmt.Printf("Control\t")
-			}
-
-			if unicode.IsDigit(r) {
-				fmt.Printf("Digit\t")
-			}
-
-			if unicode.IsGraphic(r) {
-				fmt.Printf("Graphic\t")
-			}
-
-			if unicode.IsLower(r) {
-				fmt.Printf("Lower\t")
-			}
-
-			if unicode.IsNumber(r) {
-				fmt.Printf("Number\t")
-			}
-
-			if unicode.IsPrint(r) {
-				fmt.Printf("Print\t")
-			}
-
-			if unicode.IsSpace(r) {
-				fmt.Printf("Space\t")
-			}
-
-			if unicode.IsSymbol(r) {
-				fmt.Printf("Symbol\t")
-			}
-
-			if unicode.IsTitle(r) {
-				fmt.Printf("Title\t")
-			}
-
-			if unicode.IsUpper(r) {
-				fmt.Printf("Upper\t")
-			}
-
-			if unicode.IsLetter(r) {
-				fmt.Printf("Letter\t")
-			}
-
-			if unicode.Is(unicode.Han, r) {
-				fmt.Printf("Han\t")
-			}
-
-			if unicode.Is(unicode.Hiragana, r) {
-				fmt.Printf("Hiragana\t")
-			}
-
-			if unicode.Is(unicode.Katakana, r) {
-				fmt.Printf("Katakana")
-			}
-
-			fmt.Println()
+		if err != nil {
+			return nil, err
 		}
+
+		conn.SetDeadline(time.Now().Add(rwTimeout))
+
+		return conn, nil
+	}
+}
+
+func FetchURL(theurl string) string {
+	var client *http.Client
+
+	if proxy := os.Getenv("http_proxy"); proxy != `` {
+		proxyUrl, err := url.Parse(proxy)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		transport := http.Transport{
+			Dial:  TimeoutDialer(5*time.Second, 5*time.Second),
+			Proxy: http.ProxyURL(proxyUrl),
+		}
+
+		client = &http.Client{Transport: &transport}
+	} else {
+		client = &http.Client{}
+	}
+
+	req, err := http.NewRequest(`GET`, theurl, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return string(body)
+}
+
+func main() {
+	counter := make(map[string]int, 0)
+
+	data := FetchURL(os.Args[1])
+
+	var buffer []rune
+
+	printed := false
+
+	for _, r := range data {
+		if unicode.Is(unicode.Han, r) || unicode.Is(unicode.Hiragana, r) || unicode.Is(unicode.Katakana, r) || r == 'ー' {
+			buffer = append(buffer, r)
+
+			printed = false
+		} else if printed != true {
+			printed = true
+			counter[string(buffer)] += 1
+			buffer = make([]rune, 0)
+		}
+	}
+
+	for k, v := range counter {
+		fmt.Println(v, k)
 	}
 }
